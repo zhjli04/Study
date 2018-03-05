4c32de12af574492a9814b0c03cbc8448ef0b6a0
zhjli04 <34648519+zhjli04@users.noreply.github.com>
2018-03-05 19:00:28 +0800

    Create MedianoftwoSortedArrays.java

---
+++ lintcodes/MedianoftwoSortedArrays.java
@@ -0,0 +1,49 @@
+public class Solution {
+    /*
+    There are two sorted arrays A and B of size m and n respectively. Find the median of the two sorted arrays.
+    
+    Example
+Given A=[1,2,3,4,5,6] and B=[2,3,4,5], the median is 3.5.
+
+Given A=[1,2,3] and B=[4,5], the median is 3.
+
+     * @param A: An integer array
+     * @param B: An integer array
+     * @return: a double whose format is *.5 or *.0
+     */
+    public double findMedianSortedArrays(int[] A, int[] B) {
+        // write your code here
+         int m = A.length;
+        int n = B.length;
+        if (m > n) { // to ensure m<=n
+            int[] temp = A; A = B; B = temp;
+            int tmp = m; m = n; n = tmp;
+        }
+        int iMin = 0, iMax = m, halfLen = (m + n + 1) / 2;
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = halfLen - i;
+            if (i < iMax && B[j-1] > A[i]){
+                iMin = iMin + 1; // i is too small
+            }
+            else if (i > iMin && A[i-1] > B[j]) {
+                iMax = iMax - 1; // i is too big
+            }
+            else { // i is perfect
+                int maxLeft = 0;
+                if (i == 0) { maxLeft = B[j-1]; }
+                else if (j == 0) { maxLeft = A[i-1]; }
+                else { maxLeft = Math.max(A[i-1], B[j-1]); }
+                if ( (m + n) % 2 == 1 ) { return maxLeft; }
+
+                int minRight = 0;
+                if (i == m) { minRight = B[j]; }
+                else if (j == n) { minRight = A[i]; }
+                else { minRight = Math.min(B[j], A[i]); }
+
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        return 0.0;
+    }
+}
