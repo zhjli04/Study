2de9d6e4397fb65481fbe6a3caf78fb5d63626d6
zhjli04 <34648519+zhjli04@users.noreply.github.com>
2018-03-30 15:39:46 +0800

    Update RotateImage.java

--- lintcodes/RotateImage.java
+++ lintcodes/RotateImage.java
@@ -17,6 +17,22 @@ rotate it by 90 degrees (clockwise), return
     [4,2]
 ]
 
+The idea was firstly transpose the matrix and then flip it symmetrically. For instance,
+
+1  2  3             
+4  5  6
+7  8  9
+after transpose, it will be swap(matrix[i][j], matrix[j][i])
+
+1  4  7
+2  5  8
+3  6  9
+Then flip the matrix horizontally. (swap(matrix[i][j], matrix[i][matrix.length-1-j])
+
+7  4  1
+8  5  2
+9  6  3
+
      * @param matrix: a lists of integers
      * @return: nothing
      */
