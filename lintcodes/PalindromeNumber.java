17e6cc9fa4b91f3227497c7edfeb42444842087c
zhjli04 <34648519+zhjli04@users.noreply.github.com>
2019-04-22 19:12:23 +0800

    Update PalindromeNumber.java

--- lintcodes/PalindromeNumber.java
+++ lintcodes/PalindromeNumber.java
@@ -40,5 +40,29 @@ Example
         }
         return true;
     }
-    
+}
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        
+        if (x < 10) {
+            return true;
+        }
+        
+        String s = String.valueOf(x);
+        int start = 0;
+        int end = s.length() -1;
+        
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
 }
