35b39e7a2aecb41dc74df93e02ce7455084581c6
zhjli04 <34648519+zhjli04@users.noreply.github.com>
2019-05-21 12:43:41 +0800

    Create rotateRight.java

---
+++ lintcodes/rotateRight.java
@@ -0,0 +1,72 @@
+/*
+Given a linked list, rotate the list to the right by k places, where k is non-negative.
+
+Example 1:
+
+Input: 1->2->3->4->5->NULL, k = 2
+Output: 4->5->1->2->3->NULL
+Explanation:
+rotate 1 steps to the right: 5->1->2->3->4->NULL
+rotate 2 steps to the right: 4->5->1->2->3->NULL
+Example 2:
+
+Input: 0->1->2->NULL, k = 4
+Output: 2->0->1->NULL
+Explanation:
+rotate 1 steps to the right: 2->0->1->NULL
+rotate 2 steps to the right: 1->2->0->NULL
+rotate 3 steps to the right: 0->1->2->NULL
+rotate 4 steps to the right: 2->0->1->NULL
+*/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        int len = count(head);
+        if (k >= len) {
+            k = k % len;
+        }
+        
+        if (k == 0) {
+            return head;
+        }
+        
+        ListNode cur = head;
+        int i = 0;
+        while (i < len - k - 1) {
+            cur = cur.next;
+            i++;
+        }
+        
+        ListNode newHead = cur.next;
+        cur.next = null;
+        
+        ListNode end = newHead;
+        while (end != null && end.next != null) {
+            end = end.next;
+        }
+        
+        end.next = head;
+        return newHead;
+    }
+    
+    private int count (ListNode head) {
+        int c = 0;
+        ListNode cur = head;
+        while(cur != null) {
+            c++;
+            cur = cur.next;
+        }
+        return c;
+    }
+}
