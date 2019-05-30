e6aaf7359ed2fd991f2b54f9cfc5d0a6cf167590
zhjli04 <34648519+zhjli04@users.noreply.github.com>
2019-05-30 16:10:40 +0800

    Create minPathSum.java

---
+++ lintcodes/minPathSum.java
@@ -0,0 +1,40 @@
+/*
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+
+Example:
+
+Input:
+[
+  [1,3,1],
+  [1,5,1],
+  [4,2,1]
+]
+Output: 7
+Explanation: Because the path 1→3→1→1→1 minimizes the sum.
+*/
+class Solution {
+    public int minPathSum(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0) {
+            return 0;
+        }
+        
+        int row = grid.length;
+        int col = grid[0].length;
+        for (int i=1; i < col; i++) {
+            grid[0][i] += grid[0][i-1];
+        }
+        
+        for (int i=1; i<row; i++) {
+            grid[i][0] += grid[i-1][0];
+        }
+        
+        for (int i=1; i<row; i++) {
+            for (int j=1; j<col; j++) {
+                grid[i][j] += Math.min(grid[i-1][j], grid[i][j-1]);
+            }
+        }
+        return grid[row-1][col-1];
+    }
+}
