f40b7e7a56ea5560d75034d62da74e5ea95b2d88
zhjli04 <34648519+zhjli04@users.noreply.github.com>
2019-06-21 14:39:08 +0800

    Create hasCycle.java

---
+++ lintcodes/hasCycle.java
@@ -0,0 +1,66 @@
+/*
+141. Linked List Cycle
+Easy
+
+1590
+
+173
+
+Favorite
+
+Share
+Given a linked list, determine if it has a cycle in it.
+
+To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+
+ 
+
+Example 1:
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+
+Example 2:
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where tail connects to the first node.
+
+
+Example 3:
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+*/
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return false;
+        }
+        
+        ListNode one = head;
+        ListNode two = head;
+        while (one != null && two != null && two.next != null) {
+            one = one.next;
+            two = two.next.next;
+            if (one == two) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
